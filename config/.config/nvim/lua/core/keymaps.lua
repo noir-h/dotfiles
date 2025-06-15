8518e35d3546cd1497cd5ff074c63c809dfb48ae
sora igarashi <65098540+noir-h@users.noreply.github.com>
2025-06-15 17:15:58 +0900

    update

--- config/.config/nvim/lua/core/keymaps.lua
+++ config/.config/nvim/lua/core/keymaps.lua
@@ -4,12 +4,12 @@ local opts = { noremap = true, silent = true }
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-map("n", "<Tab>", "<Cmd>BufferNext<CR>", opts) -- 次のバッファ
-map("n", "<S-Tab>", "<Cmd>BufferPrevious<CR>", opts) -- 前のバッファ
-map("n", "<leader>bc", "<Cmd>BufferClose<CR>", opts) -- 現在のバッファを閉じる
+map("n", "<Tab>", "<Cmd>BufferNext<CR>", opts)                    -- 次のバッファ
+map("n", "<S-Tab>", "<Cmd>BufferPrevious<CR>", opts)              -- 前のバッファ
+map("n", "<leader>bc", "<Cmd>BufferClose<CR>", opts)              -- 現在のバッファを閉じる
 map("n", "<leader>bo", "<Cmd>BufferCloseAllButCurrent<CR>", opts) -- 現在のバッファ以外を閉じる
-map("n", "<leader>bp", "<Cmd>BufferMovePrevious<CR>", opts) -- 左へ移動
-map("n", "<leader>bn", "<Cmd>BufferMoveNext<CR>", opts) -- 右へ移動
+map("n", "<leader>bp", "<Cmd>BufferMovePrevious<CR>", opts)       -- 左へ移動
+map("n", "<leader>bn", "<Cmd>BufferMoveNext<CR>", opts)           -- 右へ移動
 
 
 -- This file is automatically loaded by plugins.core
